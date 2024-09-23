1648410c942a829bcb00da4d466ddf2ebecf7dca
Isaac Lyons <isaac@snowskeleton.net>
2024-09-23 09:50:17 -0400

    Initial Commit

---
+++ ACS Contacts/ACS_ContactsApp.swift
@@ -0,0 +1,32 @@
+//
+//  ACS_ContactsApp.swift
+//  ACS Contacts
+//
+//  Created by snow on 9/23/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ACS_ContactsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
