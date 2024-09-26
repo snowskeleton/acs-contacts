320206009b4b2cb1d8b4c760c537deaf09b6062d
Isaac Lyons <isaac@snowskeleton.net>
2024-09-26 10:37:22 -0400

    fix addressString computation

--- ACS Contacts/Models/Address.swift
+++ ACS Contacts/Models/Address.swift
@@ -30,24 +30,24 @@ struct Address: BlackbirdModel, Identifiable {
     @BlackbirdColumn var cityStateZip: String?
     
     var addressString: String {
-        var starterString = ""
-        if let line1 = addressLine1, !line1.isEmpty {
-            starterString += "\(line1)"
+        var addressPieces: [String] = []
+        
+        if let addressLine1, !addressLine1.isEmpty {
+            addressPieces.append(addressLine1)
         }
         
-        if let line2 = addressLine2, !line2.isEmpty {
-            starterString += "\n\(line2)"
+        if let addressLine2, !addressLine2.isEmpty {
+            addressPieces.append(addressLine2)
         }
         
-        if let cityStateZip = cityStateZip, !cityStateZip.isEmpty {
-            starterString += "\n\(cityStateZip)"
+        if let cityStateZip, !cityStateZip.isEmpty {
+            addressPieces.append(cityStateZip)
         }
         
-        if let country = country, !country.isEmpty {
-            starterString += "\n\(country)"
+        if let country, !country.isEmpty {
+            addressPieces.append(country)
         }
-        
-        return starterString
+        return addressPieces.joined(separator: "\n")
     }
 
     init(from apiResponse: IndividualContactResponse.Address, for indvId: Int) {
